f9b68de85b9b63d8ff04762d43c1bb81efffbc3f
Clemens Akens <clebert@me.com>
2021-01-17 12:09:42 +0100

    Improve example

--- example.js
+++ example.js
@@ -14,8 +14,8 @@ function useGreeting(salutation) {
 
     setTimeout(() => {
       // Unlike React, Batis always applies all state changes, whether
-      // synchronous or asynchronous, in batches. Therefore, "Janie" will never
-      // be greeted individually.
+      // synchronous or asynchronous, in batches. Therefore, Janie is not
+      // greeted individually.
       setName('Janie');
       setName((prevName) => prevName + ' and Johnny');
     });
