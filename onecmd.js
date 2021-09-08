077a4b12183529bc8be0507427ed0e21d9c9b7bc
Clemens Akens <clebert@me.com>
2021-09-08 15:36:33 +0200

    Update @onecmd/standard-plugins to version 0.3.0

--- onecmd.js
+++ onecmd.js
@@ -1,14 +1,16 @@
 // @ts-check
 
 const plugins = require('@onecmd/standard-plugins');
+const nodeVersion = '16';
 
 module.exports = [
   plugins.babel(),
   plugins.editorconfig(),
   plugins.eslint(),
   plugins.git(),
+  plugins.github({branches: ['master'], nodeVersion}),
   plugins.jest({coverage: true}),
-  plugins.node('16'),
+  plugins.node(nodeVersion),
   plugins.npm(),
   plugins.prettier(),
   plugins.typescript('node', 'package'),
